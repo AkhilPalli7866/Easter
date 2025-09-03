7b2f5c872526d4c15217b209e329411a3f57812c
AkhilPalli7866 <akhilpalli786@gmail.com>
2025-09-03 17:18:30 -0400

    Update @ 5:18 PM 9/3/2025

--- Easter.java
+++ Easter.java
@@ -8,6 +8,14 @@ public class Easter{
         int c = y%100;
         int d = b/4;
         int e = b%4;
-        
+        int f = b+8/25;
+        int g = (b-f+1)/3;
+        int h = (19*a+b-d-g+15)/30;
+        int i = c/4;
+        int k = c%4;
+        int r = (32+2*e+2*i -h-k)%7;
+        int m = (a+11*h+22*r)/451;
+        int n = (h+r-7*m+144)/31;
+        int p = (h+r-7*m+144)%31;
     }
 }
