34baa4d1dc62841c967caf2cdcdbc1ba52253146
AkhilPalli7866 <akhilpalli786@gmail.com>
2025-09-03 18:30:22 +0000

    save_version2

--- Easter.java
+++ Easter.java
@@ -2,6 +2,12 @@
 //This program will calculate the date Easter falls on given a year after 1583
 public class Easter{
     public static void main(String[] args){
+        int y = 2003;
+        int a = y%19;
+        int b = y/100;
+        int c = y%100;
+        int d = b/4;
+        int e = b%4;
         
     }
 }
