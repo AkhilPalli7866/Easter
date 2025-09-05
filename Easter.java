bcc1c04033d554a25cce98402713b0aa6bb8185e
AkhilPalli7866 <akhilpalli786@gmail.com>
2025-09-05 17:36:18 +0000

    Final_Version_Easter

--- Easter.java
+++ Easter.java
@@ -1,22 +1,25 @@
 // Akhil Palli
 //This program will calculate the date Easter falls on, given a year after 1583.
+import java.util.*;
+
 public class Easter{
     public static void main(String[] args){
-        int y = 2003;
+        Scanner scanner = new Scanner(System.in);
+        int y = scanner.nextInt();
         int a = y%19;
         int b = y/100;
         int c = y%100;
         int d = b/4;
         int e = b%4;
-        int f = b+8/25;
+        int f = (b+8)/25;
         int g = (b-f+1)/3;
-        int h = (19*a+b-d-g+15)/30;
+        int h = (19*a+b-d-g+15)%30;
         int i = c/4;
         int k = c%4;
         int r = (32+2*e+2*i -h-k)%7;
         int m = (a+11*h+22*r)/451;
-        int n = (h+r-7*m+144)/31;
-        int p = (h+r-7*m+144)%31;
+        int n = (h+r-7*m+114)/31;
+        int p = (h+r-7*m+114)%31;
 
          System.out.println("a = " + a);
          System.out.println("b = " + b);
@@ -33,5 +36,8 @@ public class Easter{
          System.out.println("n = " + n);
          System.out.println("p = " + p);
         
+
+         //aDITIONAL 
+         System.out.println("Easter is on: "+ n + " / " + (p+1) +" / " + y);
     }
 }
